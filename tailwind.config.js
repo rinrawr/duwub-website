a00eb5313119dad95c5b130c68e73da57a1d59d9
dwsnb <dawson@duwub.dev>
2024-03-04 20:04:26 -0700

    Update file paths and add Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false
+  }
 }
-
