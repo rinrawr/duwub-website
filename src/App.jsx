11fd89b7fafcb631b9e7d7d37ad466117d321c30
duwub <dawson@duwub.dev>
2024-05-04 17:56:50 -0600

    init sidenav component and delete baseline css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Sidenav from './components/sidenav'
 
 function App() {
 
   return (
     <div>
-
+      <Sidenav />
     </div>
   )
 }
---
+++ src/components/sidenav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Sidenav = () => {
+    return (
+        <div>
+            <AiOutlineMenu/>
+        </div>
+    )
+}
+
+export default Sidenav
