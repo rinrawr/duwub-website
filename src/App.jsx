d9fe106f1e0b4802ed896ed705dec4691a2554d2
duwub <dawson@duwub.dev>
2024-05-04 18:35:28 -0600

    test tailwind stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import './App.css'
-import Sidenav from './components/sidenav'
+// import Sidenav from './components/sidenav'
 
 function App() {
 
   return (
-    <div>
-      <Sidenav />
+    <div className='bg-gray-900 h-screen w-screen'>
+      <section className='flex items-center justify-center h-screen'>
+        <h1 className='font-heading text-5xl sm:text-6xl md:text-7xl xl:text-9xl text-white font-semibold leading-none flex items-center justify-center'>duwub</h1>
+      </section>
     </div>
   )
 }
